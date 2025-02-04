e6135032d8110ea4a66800bd9b0e793e19d5be9e
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2025-02-05 01:18:51 +0530

    fix useSearchParams related issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+'use client';
+
+import React, { Suspense } from 'react';
 import HeroContent from '@/app/components/HeroContent';
 import Experience from '@/app/components/Experience';
 import Footer from './components/Footer';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { Lotties } from '@/assets/lotties';
 
 export default function Home() {
   return (
-    <div id="rootx" className={'xl:mx-auto max-w-[1080px] px-8'}>
-      <HeroContent />
-      <Experience />
-      <Footer />
-    </div>
+    <Suspense fallback={<DotLottieReact src={Lotties.HELLO} />}>
+      <div className={'xl:mx-auto max-w-[1080px] px-8'}>
+        <HeroContent />
+        <Experience />
+        <Footer />
+      </div>
+    </Suspense>
   );
 }
